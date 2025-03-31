3364a057dec43af7d453ed719819345d66da79ee
saksham021012 <sakshamgoyal2k4@gmail.com>
2025-04-01 03:45:33 +0530

    Update CORS configuration to allow requests from Vercel frontend

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: ['http://localhost:3000', 'https://study-notion-frontend-ten-pi.vercel.app'],
         credentials: true
     })
 )
