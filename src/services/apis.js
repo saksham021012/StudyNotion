00566139086b2827d00393e2bca211bcfdac7099
saksham021012 <sakshamgoyal2k4@gmail.com>
2025-09-06 15:34:56 +0530

    Update API configurations for environment variables and dependencies

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.REACT_APP_LOCAL_URL || process.env.REACT_APP_PROD_URL
 
 // AUTH ENDPOINTS
 export const endpoints = {
--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -48,7 +48,7 @@ export async function buyCourse(token, courses, userDetails, navigate, dispatch)
         console.log("PRINTING orderResponse", orderResponse);
         //options
         const options = {
-            key: process.env.RAZORPAY_KEY,
+            key: process.env.REACT_APP_RAZORPAY_KEY,
             currency: orderResponse.data.message.currency,
             amount: `${orderResponse.data.message.amount}`,
             order_id:orderResponse.data.message.id,
